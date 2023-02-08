8acb810e91e90d17df3dcf77f55e0b30f4e341ff
evmatv <evgenii.contact@gmail.com>
2023-02-08 03:34:55 +0200

    frontend add data origin config, update readme file

--- client/src/services/heartRateData.js
+++ client/src/services/heartRateData.js
@@ -1,9 +1,13 @@
 import axios from "axios";
 import configData from "config.json";
 
-const baseUrl = configData.DATABASE_API;
+let apiUrl = configData.DATABASE_API;
+
+if (configData.DATA_ORIGIN === "local") {
+  apiUrl = configData.LOCAL_DATA_STORAGE_API;
+}
 
 export const getHeartRateData = () => {
-  const request = axios.get(baseUrl);
+  const request = axios.get(apiUrl);
   return request.then((response) => response.data);
 };
